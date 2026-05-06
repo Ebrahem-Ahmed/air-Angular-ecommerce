62ef35c005368227fb0ce17f032050e807f72434
EBRAHEM-SHAMS <EBRAHEM-SHAMS@DESKTOP-29JJB1R>
2026-05-06 17:10:09 +0300

    ccc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://aircomfortapi.runasp.net/api',
-//apiUrl: 'http://localhost:5001/api',
+//apiUrl: 'http://localhost:5001/apis',
   appName: 'MyApp',
   supportedLanguages: ['en', 'ar'],
   googleClientId:
